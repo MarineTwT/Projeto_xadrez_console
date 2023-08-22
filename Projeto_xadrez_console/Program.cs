070c0fc573a303a29364734b06b7612ee168ce8e
Pablo <pablojdrosa@gmail.com>
2023-08-22 08:10:39 +0100

    Criação da classe tabuleiro

--- Projeto_xadrez_console/Program.cs
+++ Projeto_xadrez_console/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Projeto_xadrez_console
+﻿using Tabuleiro;
+
+namespace Projeto_xadrez_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao P;
+
+            P = new Posicao(3,4);
+
+            Console.WriteLine("posição: " + P);
+
+            Console.ReadLine();
         }
     }
 }
